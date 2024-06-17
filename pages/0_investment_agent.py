0ad5820be2b52e2db8f054e8649762d4bd39f361
Hidiebye11 <122888848+Hidiebye11@users.noreply.github.com>
2024-06-17 18:04:33 +0800

    pp

--- pages/0_investment_agent.py
+++ pages/0_investment_agent.py
@@ -1,30 +1,45 @@
 # Import the required libraries
 import streamlit as st
 from phi.assistant import Assistant
-from phi.llm.openai import OpenAIChat
+
 from phi.tools.yfinance import YFinanceTools
+from groq import Groq
 
 # Set up the Streamlit app
 st.title("AI Investment Agent 📈🤖")
 st.caption("This app allows you to compare the performance of two stocks and generate detailed reports.")
 
-# Get OpenAI API key from user
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+groq_api = st.secrets["GROQ"]
 
-if openai_api_key:
-    # Create an instance of the Assistant
-    assistant = Assistant(
-        llm=OpenAIChat(model="gpt-4o", api_key=openai_api_key),
-        tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
-        show_tool_calls=True,
-    )
+client = Groq(
+    api_key=groq_api,
+)
+def generate_response(prompt_input):
+    userPrompt = {
+        "role": "user",
+        "content": "Using all the financial tools and financial knowledge you have"
+        + ", compare the following stocks and give appropriate financial advise on what to do with the stock:"
+        + prompt_input,
+    }
+    messageLog = [userPrompt]
 
-    # Input fields for the stocks to compare
-    stock1 = st.text_input("Enter the first stock symbol")
-    stock2 = st.text_input("Enter the second stock symbol")
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=messageLog,
+            model="llama3-8b-8192",
+        )
 
-    if stock1 and stock2:
-        # Get the response from the assistant
-        query = f"Compare {stock1} to {stock2}. Use every tool you have."
-        response = assistant.run(query, stream=False)
-        st.write(response)
+    except:
+        return "Sorry I cannot repond right now!"
+    return chat_completion.choices[0].message.content
+
+
+# Input fields for the stocks to compare
+stock1 = st.text_input("Enter the first stock symbol")
+stock2 = st.text_input("Enter the second stock symbol")
+
+if stock1 and stock2:
+    # Get the response from the assistant
+    query = f"Compare {stock1} to {stock2}. Use every tool you have."
+    response = generate_response(query)
+    st.write(response)
